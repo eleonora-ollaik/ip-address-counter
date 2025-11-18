da73cddea4421f327a9b16b5ba8684a315f8de39
eleonora-ollaik <eleonora-ollaik@users.noreply.github.com>
2025-11-18 01:57:41 +0100

    fix: Smaller fixes, adding comments, removing unused code

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # ip-address-counter
-Golang program that can count and output unique number of IPv4 addresses from a given txt file.
+Golang program that can count and output **unique number of IPv4 addresses from a given txt file.
+
+## Installation
+
+Clone this repository:
+
+```git clone https://github.com/eleonora-ollaik/ip-address-counter.git```
+
+### Usage
+
+When using you **must** provide a filename when you run the programm: 
+
+```go run main.go <filename>```
+
+For example: 
+
+```go run main.go ip_addresses.txt```
+
+#### Testing:
+
+For a smaller subset use *ip_addresses.txt*.
+It has roughly ~1400 ip addresses from which only 79 are unique.
+
+*Expected output:*
+
+```go run main.go ip_addresses.txt```
+
+Starting the count...
+
+Execution complete.
+
+Unique number of IPs: 79
+Lines processed: 1472
+Time taken to execute: 4.3383ms
+
+*Unhappy scenarios:*
+
+```go run main.go non-existent-file```
+
+Error: file ip_addresses does not exist
+
+-----------------
+
+This file contains 3 strings instead of IPv4 addresses
+The program is designed to skip those lines to continue count
+```go run main.go ip_addresses_invalids.txt```
+
+Starting the count...
+
+Execution complete.
+
+Unique number of IPs: 79
+Lines processed: 1472
+Time taken to execute: 4.2913ms
--- main.go
+++ main.go
@@ -12,49 +12,65 @@ import (
 	"time"
 )
 
-type UniqueError struct {
-	Message string
-	Count   int
-	Indices []int
-}
-
 // For this case the ds used is bitmap.
 // byte array of length 536 870 912 bytes.
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <filename>")
+		fmt.Println("Error: Please run again. Usage: go run main.go <filename>")
 		return
 	}
-	filename := os.Args[1]
 	startTime := time.Now()
-	fmt.Printf("Hi\n")
-	var wg sync.WaitGroup
-	// Setting number of workers to be CPU cores number for optimal solution
-	workers := runtime.NumCPU()
+	filename := os.Args[1]
 
-	//filename := "ip_addresses.txt"
-	lines := make(chan string, 1000)
+	// fileExtention := strings.ToLower(filepath.Ext(filename))
+	// if fileExtention != ".txt" {
+	// 	fmt.Printf("Error: only .txt files are allowed\n")
+	// 	return
+	// }
 
-	var mu sync.Mutex
+	if _, error := os.Stat(filename); os.IsNotExist(error) {
+		fmt.Printf("Error: file %v does not exist\n", filename)
+		return
+	} else if error != nil {
+		fmt.Printf("Error: error checking the file: %v\n", error)
+	}
+
+	fmt.Printf("Starting the count...\n")
+	fmt.Println()
+
 	//Each number in IPv4 is a byte, meaning whole address is = 32 (4*8)
 	//To accumulate for bit state let's assume that each state is 2,
 	//then 2^32 = 4 294 967 296 <= number of possible ip addresses
 	//that means that 4 294 967 296 / 8 = 536 870 912 bytes is needed for all
 	bitmap := make([]byte, 536870912)
-	var count uint64
+
+	var ipCount uint64
+	var linesCount uint64
+	var wg sync.WaitGroup
+	var muUniqueIps sync.Mutex
+	// Setting number of workers to be CPU cores number for optimal solution
+	workers := runtime.NumCPU()
 	wg.Add(workers)
 
+	// 1000 is an arbitrary number, enough for the usecase
+	lines := make(chan string, 1000)
+
 	for i := 1; i <= workers; i++ {
-		go processor(lines, &wg, bitmap, &count, &mu)
+		go processor(lines, &wg, bitmap, &ipCount, &linesCount, &muUniqueIps)
 	}
 	go reader(filename, lines)
 	wg.Wait()
-	//count := counter(bitmap)
-	finishTime := time.Since(startTime)
-	fmt.Printf("Done, unique number is %v, time taken %v \n", count, finishTime)
 
+	finishTime := time.Since(startTime)
+	fmt.Printf("Execution complete.\n")
+	fmt.Println()
+	fmt.Printf("Unique number of IPs: %v\n", ipCount)
+	fmt.Printf("Lines processed: %v\n", linesCount)
+	fmt.Printf("Time taken to execute: %v \n", finishTime)
+	fmt.Println()
 }
 
+// Reader that goes through file and feeds each line into the channel
 func reader(filename string, lines chan<- string) {
 	startTime := time.Now()
 	file, _ := os.Open(filename)
@@ -67,53 +83,44 @@ func reader(filename string, lines chan<- string) {
 	close(lines)
 	finishTime := time.Since(startTime)
 	fmt.Printf("Time taken for reading %v \n", finishTime)
-
 }
 
-func processor(lines <-chan string, wg *sync.WaitGroup, bitmap []byte, count *uint64, mu *sync.Mutex) {
-
+// Processor function that is defining unique ip addresses from the stream
+func processor(lines <-chan string, wg *sync.WaitGroup, bitmap []byte, ipCount *uint64, linesCount *uint64, muUniqueIps *sync.Mutex) {
 	defer wg.Done()
 	startTime := time.Now()
 
 	for ipAddressString := range lines {
-		ipAddress := stringToUint32(ipAddressString)
-		// if error != nil {
-		// 	// key := error.Error()
-		// 	// if uniqueError, exists := errorMap[key]; exists {
-		// 	// 	uniqueError.Count++
-		// 	// } else {
-		// 	// 	errorMap[key] = &UniqueError{
-		// 	// 		Message: key,
-		// 	// 		Count:   1,
-		// 	// 	}
-		// 	// }
-		// 	// continue
-		// }
+		ipAddress, err := stringToUint32(ipAddressString)
+		atomic.AddUint64(linesCount, 1)
+		if err != nil {
+			return // Skip the non ip line to keep counting IPv4s
+		}
 		//Byte index to check in the bitmap:
 		byteIndex := ipAddress / 8
 		byteOffset := ipAddress % 8
 		//Bit to check:
 		mask := byte(1 << byteOffset)
-		//println("mask ", mask)
+
 		//If item doesn't exist in the bitmap, add there and increase count
-		mu.Lock()
+		muUniqueIps.Lock()
 		if bitmap[byteIndex]&mask == 0 {
 			bitmap[byteIndex] |= mask
-			atomic.AddUint64(count, 1)
+			atomic.AddUint64(ipCount, 1)
 		}
-		mu.Unlock()
-		//fmt.Println("Line: ", ipAddress)
+		muUniqueIps.Unlock()
+
 	}
 	finishTime := time.Since(startTime)
 	fmt.Printf("Time taken for processing %v \n", finishTime)
 
 }
 
-func stringToUint32(ipString string) uint32 {
+// Function that is converting ip string value to Uint32
+func stringToUint32(ipString string) (uint32, error) {
 	ipInt := net.ParseIP(ipString).To4()
 	if ipInt == nil {
-		//return 0, errors.New("invalid IPv4 address")
-		panic("Invalid ipV4 address")
+		return 0, fmt.Errorf("is not a valid IPv4 address")
 	}
-	return binary.BigEndian.Uint32(ipInt)
+	return binary.BigEndian.Uint32(ipInt), nil
 }
